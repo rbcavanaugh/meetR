f256952457a38eb8bb85fa7beae6456d88e47421
Rob Cavanaugh <rob.cavanaugh@pitt.edu>
2021-03-07 10:40:42 -0500

    links

--- www/link.R
+++ www/link.R
@@ -1,10 +1,67 @@
-link = "https://docs.google.com/spreadsheets/d/1eXXCcAsbUc5K7LSqzid3TTzDideHd6NsSqZOB7jEMqM/edit?usp=sharing"
-# user_base <- tibble(
-#   user = c("user", "admin"),
-#   password = c("password", "password"),
-#   password_hash = sapply(c("password", "password"), sodium::password_store),
-#   permissions = c("standard", "admin"),
-#   name = c("User", "Admin")
-# )
-# 
-# saveRDS(user_base, "user_base.rds")
+
+
+#link = "https://docs.google.com/spreadsheets/d/1eXXCcAsbUc5K7LSqzid3TTzDideHd6NsSqZOB7jEMqM/edit?usp=sharing"
+
+admin_name = "rob-admin"
+
+#urlpath = "mongodb+srv://shiny-user:password1234@cluster0.yt0db.mongodb.net/test?retryWrites=true&w=majority"
+
+options(mongodb = list(
+  "host" = "cluster0.yt0db.mongodb.net",
+  "username" = "shiny-user",
+  "password" = "password1234"
+))
+databaseName <- "test"
+collectionName <- "test"
+
+saveData <- function(data) {
+  #Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                  "mongodb+srv://%s:%s@%s/%s",
+                  options()$mongodb$username,
+                  options()$mongodb$password,
+                  options()$mongodb$host,
+                  databaseName
+              ),
+              options = ssl_options(weak_cert_validation = TRUE,
+                                    allow_invalid_hostname = TRUE))
+  # Insert the data into the mongo collection as a data.frame
+  data <- as_data_frame(data)
+  db$insert(data)
+}
+newData <- function(data) {
+  # Connect to the database
+  # db <- mongo(collection = collectionName,
+  #             url = sprintf(
+  #                 "mongodb+srv://%s:%s@%s/%s",
+  #                 options()$mongodb$username,
+  #                 options()$mongodb$password,
+  #                 options()$mongodb$host,
+  #                 databaseName
+  #             ),
+  #             options = ssl_options(weak_cert_validation = TRUE,
+  #                                   allow_invalid_hostname = TRUE))
+  # # Insert the data into the mongo collection as a data.frame
+  # data <- as_tibble(data)
+  # db$remove('{}')
+  # db$insert(data)
+}
+loadData <- function() {
+  # Connect to the database
+  db <- mongo(collection = collectionName,
+              url = sprintf(
+                  "mongodb+srv://%s:%s@%s/%s",
+                  options()$mongodb$username,
+                  options()$mongodb$password,
+                  options()$mongodb$host,
+                  databaseName
+              ),
+              options = ssl_options(weak_cert_validation = TRUE))
+  # Read all the entries
+  data <- db$find()
+  data
+}
+print(loadData())
+
+saveData(df)
